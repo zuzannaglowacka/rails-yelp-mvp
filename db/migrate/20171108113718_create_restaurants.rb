06ef024980cb0650073761987fa7276ac4362df6
Zuzanna Glowacka <zuzglowacka@gmail.com>
2017-11-08 11:39:35 +0000

    Add restaurant model

---
+++ db/migrate/20171108113718_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[5.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :string
+      t.integer :phone_number
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
